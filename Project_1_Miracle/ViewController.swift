21a438c08f88d3c8fdf2e2e83ea4113d28854b65
0Ceska <Guest@ceslovass-mac.local>
2016-11-17 20:14:25 +0200

    Initial Commit

---
+++ Project_1_Miracle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project_1_Miracle
+//
+//  Created by Guest User on 17/11/16.
+//  Copyright © 2016 ScriptUP Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
