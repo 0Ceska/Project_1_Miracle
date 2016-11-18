15e5ef0b35612c088a2adb92cbc5c0a4baf05c85
0Ceska <Guest@Ceslovass-Mac.local>
2016-11-18 22:42:19 +0200

    Finishedproject

--- Project_1_Miracle/ViewController.swift
+++ Project_1_Miracle/ViewController.swift
@@ -12,15 +12,22 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     //UIPickerViewDataSource  <-
     //UIPickerViewDelegate    <-
+    
+   
     @IBOutlet weak var statePicker: UIPickerView!
-    @IBOutlet weak var StatePickerBtn: UIButton!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    //
+    @IBOutlet weak var countryl: UILabel!
+    @IBOutlet weak var countryt: UITextField!
+    @IBOutlet weak var zipcodel: UILabel!
+    @IBOutlet weak var zipcodet: UITextField!
     
-    let states = ["Alaska", "Alabama", "California", "New York"]
-    
-    override func viewDidLoad() {
+    let states = ["Alabama","Arkansas","Alaska","California", "New York"]
+
+       override func viewDidLoad() {
         super.viewDidLoad()
-        statePicker.dataSource = self  //
-        statePicker.delegate = self    //
+        statePicker.dataSource = self
+        statePicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
         //First project
     }
@@ -31,28 +38,36 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         
     }
     
-    
-    @IBAction func StayeButtonPressed(_ sender: Any) { //
-        
-        statePicker.isHidden = false //
-        
+    @IBAction func StateButtonPressed(_ sender: Any) {
+        statePicker.isHidden = false
+        countryl.isHidden = true
+        countryt.isHidden = true
+        zipcodel.isHidden = true
+        zipcodet.isHidden = true
     }
     
-    
-    //
-    func numberOfComponents(in pickerView: UIPickerView) -> Int { //
-        return 1 //
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int { //function that sets the number of rows in UIViewController
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return states.count
     }
     
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? { //
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return states[row]
     }
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) { //
-        StatePickerBtn.setTitle(states[row], for: UIControlState.normal) //
-        statePicker.isHidden = true //
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+        countryl.isHidden = false
+        countryt.isHidden = false
+        zipcodel.isHidden = false
+        zipcodet.isHidden = false
     }
+    @IBAction func buynow(_ sender: Any) {
+
+    }
+    
 }
